b8e7717a856bb293878701eaa5b976d2235f58c1
wblixiang <wb.lixiang2019@mesg.corp.netease.com>
2019-10-14 11:59:30 +0800

    add project

--- src/main/java/com/dream/demo/DemoApplication.java
+++ src/main/java/com/dream/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.dream.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.dream.demo.dao")
+@EnableTransactionManagement
 public class DemoApplication {
 
     public static void main(String[] args) {
